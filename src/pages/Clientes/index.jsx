275db2ddcf472b726f7fa13b88da1bdf30baef76
NetoMarcal <neto.marcal.dev@gmail.com>
2022-07-06 16:57:39 -0300

    Testes manipulação lista clientes

--- src/pages/Clientes/index.jsx
+++ src/pages/Clientes/index.jsx
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getClients } from '../../api/Clients/Clientes';
+
 import Menu from '../../components/Menu';
 
-export default class Clients extends Component {
 
+const Clients = () => {
+
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+
+        showList();
+
+    }, []);
+
+    const showList = async () => {
     
+        try {
+          
+            const resp = await getClients();
 
-    render() {
-        return(
-            <div>
-                <Menu />
-                <h3>Clientes</h3>
-                <hr className="my-3"/>
-            </div>
-        )
+            if (resp.status === 201) {
+                setList([...resp.data.clients]);
+            }
+    
+        } catch (error) {
+    
+         console.log(error);      
+        }
     }
+
+    return(
+        <div>
+            <Menu />
+            Clientes: {
+                    list.map((client)=>(
+                        <li key={client.id_client}>
+                            {client.description}
+                        </li>
+                    ))
+                } 
+            <hr className="my-3"/>
+        </div>
+    )
+
 }
+
+export default Clients;
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,48 +1,14 @@
-import React, {useState, useEffect } from 'react';
+import React from 'react';
 import Menu from '../../components/Menu';
-import { getClients } from '../../api/Clients/Clientes';
 
 const Dashboard = () => {
-
-    const [list, setLis] = useState();
-
-    useEffect(() => {
-
-        showList();
-      }, []);
-
-    const showList = async () => {
-    
-        try {
-          
-            const resp = await getClients();
-             
-          if (resp.status === 201) {
-                console.log(resp.data.clients[0].description)
-              setLis([...resp.data]);  
-          }
-    
-        } catch (error) {
-    
-         console.log(error);      
-        }
-      }
-
-     const handleSubmit = async (e) => {
-        e.preventDefault(); 
-
-       
-    }
-
-    return(
-        <div>
-            <Menu title='Dashboard' />
-            <hr className="my-3"/>
-            <p>Cliente: { }</p>
-        </div>
-    )
-
+  return(
+      <div>
+          <Menu />
+          <hr className="my-3"/>
+          <p>Dashboard</p>
+      </div>
+  )
 }
-
 
 export default Dashboard;
